93901e666a346906c8ed7e82b345f26cdb9ab02f
鱼丸粗面666 <2506930314@qq.com>
2019-01-28 01:18:32 +0800

    initial

--- ImageBox/MainWindow.xaml.cs
+++ ImageBox/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,62 @@ namespace ImageBox
     {
         public MainWindow()
         {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
         }
+
+
+        private void Open_Image(object sender, RoutedEventArgs e)
+        {
+            // 创建一个打开文件的对话框
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // 设置对话框的起始打开路径
+            openFileDialog.InitialDirectory = @"D:\Users\Teemo Nicolas\Downloads";
+
+            // 设置打开文件的类型，注意过滤器语法
+            openFileDialog.Filter = "图片|*.jpg|全部|*";
+
+            // 调用ShowDialog()方法显示改对话框，该方法的返回值代表用户是否点击了确定按钮
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    image.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                }
+                catch (NotSupportedException exception)
+                {
+                    MessageBox.Show(exception.Message, "打开文件失败", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                }
+
+            }
+            else
+            {
+                image.Source = new BitmapImage(new Uri(@"Resources/giraffe.jpg", UriKind.Relative));
+            }
+
+        }
+
+        private void Generate_Value(object sender, RoutedEventArgs e)
+        {
+            int a = 1;
+
+            value_x.Content = a;
+            value_y.Content = 2;
+
+            value_length.Content = "3";
+            value_height.Content = "4";
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            image.Source = new BitmapImage(new Uri(@"Resources/giraffe.jpg", UriKind.Relative));
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            image.Source = null;
+        }
+
     }
 }
