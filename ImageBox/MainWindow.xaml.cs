b808fd5f9ee5f1bceb7716410964e749e4363a4e
鱼丸粗面666 <2506930314@qq.com>
2019-01-29 00:25:23 +0800

    初步完善

--- ImageBox/MainWindow.xaml.cs
+++ ImageBox/MainWindow.xaml.cs
@@ -21,64 +21,85 @@ namespace ImageBox
     /// </summary>
     public partial class MainWindow : Window
     {
+        // private String fileName;
+        private BitmapImage bitmapImage;
+
         public MainWindow()
         {
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
         }
 
-
         private void Open_Image(object sender, RoutedEventArgs e)
         {
-            // 创建一个打开文件的对话框
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-            // 设置对话框的起始打开路径
-            openFileDialog.InitialDirectory = @"D:\Users\Teemo Nicolas\Downloads";
-
-            // 设置打开文件的类型，注意过滤器语法
-            openFileDialog.Filter = "图片|*.jpg|全部|*";
-
-            // 调用ShowDialog()方法显示改对话框，该方法的返回值代表用户是否点击了确定按钮
-            if (openFileDialog.ShowDialog() == true)
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"D:\Users\Teemo Nicolas\Downloads",  // 设置对话框的起始打开路径
+                Filter = "图片|*.jpg|全部|*"  // 设置打开文件的类型，注意过滤器语法
+            };  // 创建一个打开文件的对话框
+            if (openFileDialog.ShowDialog() == true)  // 调用ShowDialog()方法显示改对话框，该方法的返回值代表用户是否点击了确定按钮
             {
                 try
                 {
-                    image.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                    this.LoadImage(openFileDialog.FileName);
                 }
                 catch (NotSupportedException exception)
                 {
                     MessageBox.Show(exception.Message, "打开文件失败", MessageBoxButton.OKCancel, MessageBoxImage.Error);
                 }
-
             }
             else
             {
-                image.Source = new BitmapImage(new Uri(@"Resources/giraffe.jpg", UriKind.Relative));
+                //File_Name.Text = "giraffe.jpg";
+                //CurrentImage.Source = new BitmapImage(new Uri("Resources\\giraffe.jpg", UriKind.Relative));
+                LoadImage("Resources\\giraffe.jpg");
             }
 
         }
 
-        private void Generate_Value(object sender, RoutedEventArgs e)
+        private void LoadImage(string fileName)
         {
-            int a = 1;
-
-            value_x.Content = a;
-            value_y.Content = 2;
-
-            value_length.Content = "3";
-            value_height.Content = "4";
+            this.bitmapImage = new BitmapImage(new Uri(fileName, UriKind.RelativeOrAbsolute));
+            string[] path = fileName.Split('\\');
+            File_Name.Text = path[path.Length-1];
+            CurrentImage.Source = this.bitmapImage;
+            Image_Width.Content = this.bitmapImage.Width;
+            Image_Height.Content = this.bitmapImage.Height;
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        private void ClearImage()
         {
-            image.Source = new BitmapImage(new Uri(@"Resources/giraffe.jpg", UriKind.Relative));
+            CurrentImage.Source = null;
+            File_Name.Text = null;
+            Image_Width.Content = 0;
+            Image_Height.Content = 0;
         }
 
-        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) { LoadImage("Resources\\giraffe.jpg"); }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e) { ClearImage(); }
+
+        private void Clear_Image(object sender, RoutedEventArgs e) { ClearImage(); }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
         {
-            image.Source = null;
+            double width;
+            double height;
+            try
+            {
+                width = this.bitmapImage.Width;
+                height = this.bitmapImage.Height;
+            }
+            catch
+            {
+                width = CurrentImage.Width;
+                height = CurrentImage.Height;
+            }
+
+            Value_X.Content = (int)(e.GetPosition(CurrentImage).X * width / CurrentImage.Width);
+            Value_Y.Content = (int)(e.GetPosition(CurrentImage).Y * height / CurrentImage.Height);
         }
 
+        
     }
 }
