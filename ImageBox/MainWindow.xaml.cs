fb78b034d626e03920566323ec6b9e96b406d145
yuwancumian666 <2506930314@qq.com>
2019-02-09 17:39:30 +0800

    no message

--- ImageBox/MainWindow.xaml.cs
+++ ImageBox/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -41,32 +42,41 @@ namespace ImageBox
             };  // 创建一个打开文件的对话框
             if (openFileDialog.ShowDialog() == true)  // 调用ShowDialog()方法显示改对话框，该方法的返回值代表用户是否点击了确定按钮
             {
-                try
-                {
-                    this.LoadImage(openFileDialog.FileName);
-                }
-                catch (NotSupportedException exception)
-                {
-                    MessageBox.Show(exception.Message, "打开文件失败", MessageBoxButton.OKCancel, MessageBoxImage.Error);
-                }
+                LoadImage(openFileDialog.FileName);
             }
             else
             {
                 //File_Name.Text = "giraffe.jpg";
                 //CurrentImage.Source = new BitmapImage(new Uri("Resources\\giraffe.jpg", UriKind.Relative));
-                LoadImage("Resources\\giraffe.jpg");
+                LoadDefaultImage();
             }
-
         }
 
         private void LoadImage(string fileName)
         {
-            this.bitmapImage = new BitmapImage(new Uri(fileName, UriKind.RelativeOrAbsolute));
+            try
+            {
+                this.bitmapImage = new BitmapImage(new Uri(fileName, UriKind.RelativeOrAbsolute));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "不支持的文件类型", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+            }
             string[] path = fileName.Split('\\');
             File_Name.Text = path[path.Length-1];
             CurrentImage.Source = this.bitmapImage;
             Image_Width.Content = this.bitmapImage.Width;
             Image_Height.Content = this.bitmapImage.Height;
+            // ShowDefaultImage.IsChecked = false;
+        }
+
+        private void LoadDefaultImage()
+        {
+            File_Name.Text = "giraffe.jpg";
+            BitmapImage bitmap = new BitmapImage(new Uri("Resources\\giraffe.jpg", UriKind.Relative));
+            CurrentImage.Source = bitmap;
+            Image_Width.Content = bitmap.Width;
+            Image_Height.Content = bitmap.Height;
         }
 
         private void ClearImage()
@@ -75,15 +85,16 @@ namespace ImageBox
             File_Name.Text = null;
             Image_Width.Content = 0;
             Image_Height.Content = 0;
+            ShowDefaultImage.IsChecked = false;
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e) { LoadImage("Resources\\giraffe.jpg"); }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) { LoadDefaultImage(); }
 
         private void CheckBox_Unchecked(object sender, RoutedEventArgs e) { ClearImage(); }
 
         private void Clear_Image(object sender, RoutedEventArgs e) { ClearImage(); }
 
-        private void OnMouseMove(object sender, MouseEventArgs e)
+        private void OnMouseMove (object sender, MouseEventArgs e)
         {
             double width;
             double height;
@@ -97,20 +108,51 @@ namespace ImageBox
                 width = CurrentImage.Width;
                 height = CurrentImage.Height;
             }
-
+            // TODO
             Value_X.Content = (int)(e.GetPosition(CurrentImage).X * width / CurrentImage.Width);
             Value_Y.Content = (int)(e.GetPosition(CurrentImage).Y * height / CurrentImage.Height);
         }
 
-        private void OnDragEnter(object sender, DragEventArgs e)
+        private void OnDragEnter (object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
-                MessageBox.Show(((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString());
+                //MessageBox.Show();
+                LoadImage(((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString());
             }
         }
 
+        private void Debug_Button_Click (object sender, RoutedEventArgs e)
+        {
+            Debug_Info.Content = ((int)CurrentImage.ActualWidth).ToString() + ", " + ((int)CurrentImage.ActualHeight).ToString();
+        }
+
+        private void Flip_Image(object sender, RoutedEventArgs e)
+        {
+            // Cv2.Transpose(InputArray【Mat】 src, OutputArray dst);
+            // Cv2.Flip(InputArray src, OutputArray dst, FlipMode flipCode);
+            
+        }
+
+        /*public static Bitmap rotateImage(Bitmap b, float angle)
+        {
+            //create a new empty bitmap to hold rotated image
+            Bitmap returnBitmap = new Bitmap(b.Width, b.Height);
+            //make a graphics object from the empty bitmap
+            Graphics g = Graphics.FromImage(returnBitmap);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            //move rotation point to center of image
+            g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
+            //rotate
+            g.RotateTransform(angle);
+            //move image back
+            g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
+            //draw passed in image onto graphics object
+            g.DrawImage(b, new System.Windows.Point(0, 0));
+            return returnBitmap;
+        }*/
+
         // TODO 已经解决了拖拽问题；1. 没有显示图片也可以拖拽（Image的尺寸）；2. 拖拽选框、计算长宽及原点
-        
+
     }
 }
